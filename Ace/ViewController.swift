0717390cd97a33e56283459ca6ffd18c5948ab36
Jessica Liang <jeliang@berkeley.edu>
2020-08-14 21:48:10 -0700

    Initial Commit

--- Ace/ViewController.swift
+++ Ace/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Image(homescreen)
     }
 
 
 }
 
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
